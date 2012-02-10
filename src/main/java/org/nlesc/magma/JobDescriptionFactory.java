d9aafcf0aeddf4d841fed0c28e06457b70a56f3c
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2012-02-10 17:52:14 +0100

    started with remote wrapped job

--- src/main/java/org/nlesc/magma/JobDescriptionFactory.java
+++ src/main/java/org/nlesc/magma/JobDescriptionFactory.java
@@ -1,8 +1,12 @@
 package org.nlesc.magma;
 
 import org.gridlab.gat.GAT;
+import org.gridlab.gat.Preferences;
+import org.gridlab.gat.URI;
 import org.gridlab.gat.resources.JobDescription;
 import org.gridlab.gat.resources.SoftwareDescription;
+import org.gridlab.gat.resources.WrapperJobDescription;
+import org.gridlab.gat.resources.WrapperSoftwareDescription;
 import org.nlesc.magma.entities.JobSubmitRequest;
 
 public class JobDescriptionFactory {
@@ -50,6 +54,42 @@ public class JobDescriptionFactory {
 
 	        JobDescription jd = new JobDescription(sd);
 	        return jd;
+		} else if (jobsubmission.jobtype.equals("mzxmlremote")) {
+	        WrapperSoftwareDescription wsd = new WrapperSoftwareDescription();
+	        wsd.setStdout(GAT.createFile("wrapper.stdout"));
+	        wsd.setStderr(GAT.createFile("wrapper.stderr"));
+	        wsd.setExecutable("/usr/local/package/jdk1.6.0/bin/java");
+
+	        WrapperJobDescription wjd = new WrapperJobDescription(wsd);
+
+	        SoftwareDescription sd = new SoftwareDescription();
+	        // TODO stage magma program tarball
+	        // TODO replace args[0] with staged shell script
+	        sd.setExecutable("/usr/bin/env");
+	        String [] args = {
+	        		"mscore_mzxml", // expects mscore_mzxml to be in path
+	        		// "echo",
+					"-p", jobsubmission.arguments.precision,
+					"-c", jobsubmission.arguments.mscutoff,
+					"-d", jobsubmission.arguments.msmscutoff,
+					"-i", jobsubmission.arguments.ionisation,
+					"-n", jobsubmission.arguments.nsteps,
+					"-m", jobsubmission.arguments.phase,
+					// input files
+					"-f", jobsubmission.jobdir+"/data.mzxml",
+					"-s", jobsubmission.jobdir+"/smiles.sd",
+					// output file
+					"-o", jobsubmission.jobdir+"/results.db"
+			};
+	        sd.setArguments(args);
+	        sd.setStderr(GAT.createFile("stderr.txt"));
+            sd.setStdout(GAT.createFile("stdout.txt"));
+            Preferences preferences = new Preferences();
+            preferences.put("resourcebroker.adaptor.name", "local");
+            wjd.add(new JobDescription(sd), new URI("any://localhost"),
+                    preferences);
+
+	        return wjd;
 		} else {
 			throw new Exception("Unknown job type: '" + jobsubmission.jobtype+"'");
 		}
--- src/test/java/org/nlesc/magma/JobDescriptionFactoryTest.java
+++ src/test/java/org/nlesc/magma/JobDescriptionFactoryTest.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import static org.junit.Assert.*;
 
 import org.gridlab.gat.resources.JobDescription;
+import org.gridlab.gat.resources.WrapperJobDescription;
 import org.junit.Test;
 import org.nlesc.magma.entities.JobSubmitRequest;
 import org.nlesc.magma.entities.MzxmlArguments;
@@ -95,6 +96,65 @@ public class JobDescriptionFactoryTest extends TestCase {
 			assertArrayEquals(jd.getSoftwareDescription().getArguments(), jobargs);
 			assertEquals(jd.getSoftwareDescription().getStdout().getAbsolutePath(), jobdir+"/stdout.txt");
 			assertEquals(jd.getSoftwareDescription().getStderr().getAbsolutePath(), jobdir+"/stderr.txt");
+		} catch (Exception e) {
+			fail("Mzxml local job type must exist");
+		}
+	}
+
+	@Test
+	public void testGetJobDescriptionMzxmlRemote() {
+		JobDescriptionFactory fact = new JobDescriptionFactory();
+
+		// When user submits a magma job the website will put
+		// the user supplied mzxml file, smiles file and config file into a jobdir.
+		// then do a rest call to this service
+		// which should if broker is local
+		// 1. run mscore_mzxml with parameters in config file and files in jobdir
+		// 2. run sygma with parameters in config file
+		// 3. write completed flag to jobdir
+		// if broker is remote then
+		// 1. sync jobdir to srm
+		// 2. submit wrapper job and inside wrapper job do
+		// 2.1 sync jobdir from srm to localdir
+		// 2.2 fetch program tarball from srm
+		// 2.3 unpack program tarball
+		// 2.3.1 run progs same as for local broker
+		// 2.4 sync localdir to srm
+		// 3. sync jobdir from srm
+		// 4. clean srm
+		// 5. write completed flag to jobdir
+
+		String jobdir = System.getProperty("java.io.tmpdir")+"/"+UUID.randomUUID().toString();
+
+		MzxmlArguments arguments = new MzxmlArguments();
+		// arguments for mzxml
+		arguments.precision= "0.01";
+		arguments.mscutoff = "2e5";
+		arguments.msmscutoff = "0.1";
+		arguments.ionisation = "pos";
+		// arguments for sygma
+		arguments.nsteps = "2";
+		arguments.phase = "12";
+
+		JobSubmitRequest jobsubmission = new JobSubmitRequest(jobdir, "mzxmlremote", arguments);
+		try {
+			WrapperJobDescription wrappedjd = (WrapperJobDescription) fact.getJobDescription(jobsubmission);
+			JobDescription jd = wrappedjd.getJobInfos().get(0).getJobDescription();
+
+			assertEquals(jd.getSoftwareDescription().getExecutable(), "/usr/bin/env");
+			String[] jobargs = {
+					"mscore_mzxml",
+					"-p", arguments.precision,
+					"-c", arguments.mscutoff,
+					"-d", arguments.msmscutoff,
+					"-i", arguments.ionisation,
+					"-n", arguments.nsteps,
+					"-m", arguments.phase,
+					"-f", jobdir+"/data.mzxml",
+					"-s", jobdir+"/smiles.sd",
+					"-o", jobdir+"/results.db"
+			};
+			assertArrayEquals(jd.getSoftwareDescription().getArguments(), jobargs);
 		} catch (Exception e) {
 			fail("Mzxml local job type must exist");
 		}
