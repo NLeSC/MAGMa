99f68dbb41de559d3e47468ffa8c5c3df11c4e58
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2012-09-28 15:59:12 +0200

    Pass a ordered list of visible column names to server (refs #5).

--- web/magmaweb/static/app/view/metabolite/List.js
+++ web/magmaweb/static/app/view/metabolite/List.js
@@ -117,10 +117,17 @@ Ext.define('Esc.magmaweb.view.metabolite.List', {
     this.callParent(arguments);
   },
   /**
+   * @return {Ext.ux.grid.FiltersFeature}
+   * @private
+   */
+  getFilter: function() {
+	return this.getView().getFeature('mfilter');
+  },
+  /**
    * Clears all filters applied to metabolites
    */
   clearFilters: function() {
-    this.getView().getFeature('mfilter').clearFilters();
+    this.getFilter().clearFilters();
   },
   /**
    * Get column with scores.
@@ -171,5 +178,18 @@ Ext.define('Esc.magmaweb.view.metabolite.List', {
   showFragmentScoreColumn: function() {
     this.getFragmentScoreColumn().show();
     this.getFragmentDeltaPpmColumn().show();
+  },
+  /**
+   * @return {String} JSON encoded the filter data as query
+   */
+  getFilterQuery: function() {
+	  var filter = this.getFilter();
+	  return filter.buildQuery(filter.getFilterData());
+  },
+  /**
+   * @return {Array} Array of dataindexes currently visible. In order they appear.
+   */
+  getVisiblColumnIndices: function() {
+	  return this.getView().getHeaderCt().getVisibleGridColumns().map(function(v) {return v.dataIndex}).filter(function(v) { return v});
   }
 });
