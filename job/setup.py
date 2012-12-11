481f73815a2389aa614ed2898ab9e1c5cd2f25f1
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2012-12-11 11:16:20 +0100

    Documented interdependencies of subprojects. Added usage documentation to job/README.rst and pubchem/README.rst. Added RDKit with INCHI support check to job/setup.py. Added classifiers to setup.py. Added optional argument to process_pubchem.py for a directory with PubChem SD-files.

--- job/setup.py
+++ job/setup.py
@@ -9,6 +9,13 @@ try:
 except IOError:
     README = ''
 
+# Test if rdkit is present with INCHI support
+try:
+    from rdkit.Chem.inchi import INCHI_AVAILABLE
+    if not INCHI_AVAILABLE:
+        raise Exception('RDKit with INCHI support is required')
+except ImportError:
+    raise Exception('RDKit with INCHI support is required')
 
 # Only use Cython if it is available, else just use the pre-generated files
 try:
@@ -32,6 +39,12 @@ setup(
     url='http://www.esciencecenter.nl',
     description='Ms Annotation based on in silico Generated Metabolites',
     long_description=README,
+    classifiers=["Intended Audience :: Science/Research",
+                   "Environment :: Console",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Topic :: Scientific/Engineering :: Chemistry",
+                   ],
     packages=find_packages(),
     install_requires=['sqlalchemy', 'lxml', 'numpy', 'pp'],
     package_data={
