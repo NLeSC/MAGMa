7ee73be3344aac54cc86bee8b4ed7426badb2054
Lars Ridder <lars.ridder@wur.nl>
2013-02-06 15:09:47 +0100

    Read and store msms_intensity_cutoff as percentage, not fraction of base peak (#110)

--- job/magma/script/__init__.py
+++ job/magma/script/__init__.py
@@ -82,7 +82,7 @@ class MagmaCommand(object):
         sc.add_argument('-p', '--mz_precision', help="Mass precision (ppm) for matching calculated masses with peaks (default: %(default)s)", default=5,type=float)
         sc.add_argument('-q', '--mz_precision_abs', help="Mass precision (Da) for matching calculated masses with peaks (default: %(default)s)", default=0.001,type=float)
         sc.add_argument('-c', '--ms_intensity_cutoff', help="Minimum intensity of level 1 peaks to be annotated (default: %(default)s)", default=1e6,type=float)
-        sc.add_argument('-d', '--msms_intensity_cutoff', help="Minimum intensity of of fragment peaks to be annotated, as fraction of basepeak (default: %(default)s)", default=0.1,type=float)
+        sc.add_argument('-d', '--msms_intensity_cutoff', help="Minimum intensity of of fragment peaks to be annotated, as percentage of basepeak (default: %(default)s)", default=5,type=int)
         sc.add_argument('-i', '--ionisation_mode', help="Ionisation mode (default: %(default)s)", default="1", choices=["-1", "1"])
         sc.add_argument('-j', '--metids', type=argparse.FileType('rb'), help="File with structure ids")
         sc.add_argument('-b', '--max_broken_bonds', help="Maximum number of bond breaks to generate substructures (default: %(default)s)", default=3,type=int)
