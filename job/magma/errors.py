421ba992065e3739021b683666844c69b83c7e8a
Lars Ridder <lars.ridder@wur.nl>
2015-03-25 13:04:04 +0100

    Add errors.py (#241)

---
+++ job/magma/errors.py
@@ -0,0 +1,8 @@
+class FileFormatError(Exception):
+    'Raised when an error is found in an input file'
+    pass
+
+class DataProcessingError(Exception):
+    'Raised when an error occurs during processing of the data'
+    pass
+
